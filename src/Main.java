a2114e76e03ea54ffde7e6bb4d2d35394849b7c4
valen <dellaso.valen@gmail.com>
2024-06-14 12:15:41 -0300

    se agrego branch arraylistfunciones

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//Una aplicación de agenda de contactos necesita gestionar una lista de nombres y números de teléfono.
+// Se debe poder añadir nuevos contactos, buscar contactos por nombre y eliminar contactos.
+
+//Tareas:
+//Crea un ArrayList para almacenar nombres de contactos y otro para almacenar sus números de teléfono.
+//Implementa una función para añadir un nuevo contacto.
+//Implementa una función para buscar un contacto por nombre y mostrar su número de teléfono.
+//Implementa una función para eliminar un contacto por nombre.
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<String> NombresDeContactos = new ArrayList<String>();
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+
+    public String nuevoNombreContacto (){
+        return String nuevoNombreContacto = scanner.next();
+        contactoNombre.add(nuevoNombreContacto);
     }
 }
+
+
+
